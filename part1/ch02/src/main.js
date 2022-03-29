9cd311a3b90c3575d5293927af3dea1bad5ebbf4
K-SHINHYE <kshinhye1102@gmail.com>
2022-03-29 17:18:43 +0900

    vue route

--- part1/ch02/src/main.js
+++ part1/ch02/src/main.js
@@ -1,8 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router';
+
+// 1. Define route components.
+import HelloWorld from './components/HelloWorld.vue';
+import LoginPage from './pages/LoginPage.vue';
+// const Foo = { template: "<div>foo</div>" };
+// const Bar = { template: "<div>bar</div>" };
+
+// 2. Define some routes
+const routes = [
+  { path: "/", component: HelloWorld },
+  { path: "/login", component: LoginPage }
+];
 
 Vue.config.productionTip = false
 
+// 3. Create the router instance and pass the `routes` option
+const router = new VueRouter({
+  routes
+});
+
+// router를 쓴다고 선언하기
+Vue.use(VueRouter);
 new Vue({
+  router,
+  HelloWorld,
+  LoginPage,
   render: h => h(App),
 }).$mount('#app')
