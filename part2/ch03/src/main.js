269bebb67847d342efa7acd860e32d90a5fc5611
K-SHINHYE <kshinhye1102@gmail.com>
2022-03-30 12:54:38 +0900

    ✨ Main Page vue-router #5

--- part2/ch03/src/main.js
+++ part2/ch03/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import MainPage from './pages/MainPage.vue'
 
 Vue.config.productionTip = false
+// Vue Router사용한다고 선언해주기
+Vue.use(VueRouter);
+
+const routes = [{
+  path: '/', component: MainPage
+}];
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ part2/ch03/src/pages/MainPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ul>
+      <li><router-link to="/">Main</router-link></li>
+      <li><router-link to="/login">Login</router-link></li>
+      <li><router-link to="/profile">Profile</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default{
+  // data(){} = data:function(){}
+  data(){
+    return {
+      // monitoring해주는 값을 여기에 적어주면 됨
+    }
+  }
+}
+</script>
