1dbfa6cc04e92d7edb12d51bac674da7a4416641
K-SHINHYE <kshinhye1102@gmail.com>
2022-03-31 11:09:54 +0900

    ✨ vue3 #11

--- part3/vue3/src/App.vue
+++ part3/vue3/src/App.vue
@@ -5,8 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <img alt="Vue logo" src="./assets/춘식01.png" />
+  <HelloWorld msg="ಥ_ಥ" />
 </template>
 
 <style>
